5929b0cb7c89df99c5dc1444182c7c7e1f3c63b6
muhd shazin <50817133+drafthacker2889@users.noreply.github.com>
2022-03-20 23:00:13 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,24 @@
+<?php
+    include "header.html";
+    include "navbar.php";
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="about_style.css">
+</head>
+
+<body>
+<div class="about-section">
+  <h1>About Us</h1>
+  <p><stong>this website is created my muhammed shazin sadhik kp using HTML, CSS, PHP, MYSql</strong></p>
+  <p></p>
+  <p><strong>our website name is ACE bank, we are working thorough out the countries and we have open new branch in abu dhabi recently</strong></p>
+  <p><strong>using our website users can transfer money to diffrent other user without delay and it is secured</strong></p>
+
+</div>
+</body>
+</html>
+
+<?php include "easter_egg.php"; ?>
